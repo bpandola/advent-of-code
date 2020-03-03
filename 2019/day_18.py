cb3c5a21e0cfa6a1d95e13d681b5570ea7fcca8f
Brian Pandola <bpandola@hsdp.io>
2020-03-03 13:26:57 -0800

    Day 18, Still a Mess That Takes Too Long

--- 2019/day_18.py
+++ 2019/day_18.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import string
 
+
 def translate_input(data):
     # Translate into a more convenient data structure.
     # defaultdict[(x, y)] = data; default is '.'
@@ -20,7 +21,75 @@ def translate_input(data):
                     doors[c] = (x, y)
                 elif c in string.ascii_lowercase:
                     keys[c] = (x, y)
-    return translated, start_coords, doors, keys
+    poi = {'ROOT': fill_area_with_oxygen(translated, start_coords)}
+    for key, coords in keys.items():
+        poi[key] = fill_area_with_oxygen(translated, coords)
+    for key, coords in doors.items():
+        poi[key] = fill_area_with_oxygen(translated, coords)
+    return translated, poi, start_coords
+
+def poi_to_route(poi):
+    num_keys = len([key for key in poi.keys() if key in string.ascii_lowercase])
+
+
+    routes = [[('ROOT',0)]]
+
+    key_routes = []
+    while routes:
+        routes_copy = routes[:]
+        routes = []
+        for route in routes_copy:
+            start = route[-1]
+            moves = poi[start[0]]  #get_viable_moves(start)
+            # If there were no moves, we hit a dead end.
+            for move in moves:
+                node = move[0]
+                if len(route) > 1 and node == route[-2][0]:
+                    if list([node for node, _ in route]).count(route[-1][0]) > 1:
+                        continue  # no backtracking unless we're standing on a key/door for first time
+
+                    # if node in string.ascii_lowercase and route[-1][0] in string.ascii_uppercase:
+                    #     continue  # no backtracking from a door to a previous key
+                    # elif node in string.ascii_uppercase and route[-1][0] in string.ascii_lowercase:
+                    #     if list([node for node, _ in route]).count(route[-1][0]) > 1:
+                    #         continue  # no backtracking from a key if we've already picked it up.
+                elif node in string.ascii_lowercase:
+                    keys_acquired = set([key for key, _ in route if key in string.ascii_lowercase])
+                    keys_acquired.add(node)
+                    route_keys = len(keys_acquired)
+                    if route_keys == num_keys:
+                        key_routes.append(route + [move])
+                        continue
+                elif node in string.ascii_uppercase:
+                    if node.lower() not in [key for key, _ in route]:
+                        continue
+                routes.append(route + [move])
+
+    return key_routes
+
+
+def fill_area_with_oxygen(area_map, start_coords):
+    area = area_map.copy()
+    step = 1
+    spread_points = [start_coords]
+    area[start_coords] = '#'
+    poi = []
+    while spread_points:
+        spread_points_new = []
+        for x, y in spread_points:
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                data = area[(x + dx ,y + dy)]
+                if data == '.':
+                    spread_points_new.append((x + dx, y + dy))
+                    area[(x + dx, y + dy)] = '#'
+                elif data in string.ascii_letters:
+                    poi.append((data, step))
+                    area[(x + dx, y + dy)] = '#'
+
+        spread_points = spread_points_new
+        step += 1
+
+    return poi
 
 
 def get_viable_routes(area, pos, keys, routes=None):
@@ -124,7 +193,7 @@ def dfs(graph, doors, keys, path, viable_routes=None):
 
     return viable_routes
 
-def find_shortest_path(data):
+def dfs_shortest_path(data):
     translated, start_coords, doors, keys = translate_input(data)
     path = [start_coords]
     viable_paths = dfs(translated, doors, keys, path)
@@ -134,15 +203,11 @@ def find_shortest_path(data):
     return shortest - 1
 
 
-def dfs_shortest_path(data):
-    translated, start_coords, doors, keys = translate_input(data)
-    paths = []
-    key_routes = get_viable_routes(translated, start_coords, keys)
-    while key_routes:
-        paths = key_routes
-        key_routes = get_viable_routes(translated, start_coords, keys, key_routes)
-    shortest = min([len(p) for p in paths])
-    return shortest - 1
+def find_shortest_path(data):
+    translated, poi, start_coords = translate_input(data)
+    key_routes = poi_to_route(poi)
+    shortest = min([sum([step[1] for step in route]) for route in key_routes])
+    return shortest
 
 
 
@@ -191,10 +256,10 @@ if __name__ == '__main__':
         '########################',
     ]
     # Part 1
-    assert find_shortest_path(sample_0) == 4
-    assert find_shortest_path(sample_1) == 86
-    assert find_shortest_path(sample_2) == 132
-    #assert find_shortest_path(sample_3) == 136
-    assert find_shortest_path(sample_4) == 81
+    # assert find_shortest_path(sample_0) == 4
+    # assert find_shortest_path(sample_1) == 86
+    # assert find_shortest_path(sample_2) == 132
+    # #assert find_shortest_path(sample_3) == 136
+    #assert find_shortest_path(sample_4) == 81
 
-    #print(find_shortest_path(puzzle_input))
+    print(find_shortest_path(puzzle_input))
