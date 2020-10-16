282141e0f708143d5fb43e9e76bab6f25f88a755
Brian Pandola <bpandola@hsdp.io>
2020-10-15 23:43:18 -0700

    Day 18, Still a Mess That Takes Too Long

--- 2019/day_18.py
+++ 2019/day_18.py
@@ -26,7 +26,83 @@ def translate_input(data):
         poi[key] = fill_area_with_oxygen(translated, coords)
     for key, coords in doors.items():
         poi[key] = fill_area_with_oxygen(translated, coords)
-    return translated, poi, start_coords
+    return translated, poi, start_coords, keys, doors
+
+
+class Graph:
+    def __init__(self):
+        """
+        self.edges is a dict of all possible next nodes
+        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
+        self.weights has all the weights between two nodes,
+        with the two nodes as a tuple as the key
+        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
+        """
+        self.edges = defaultdict(list)
+        self.weights = {}
+
+    def add_edge(self, from_node, to_node, weight):
+        # Note: assumes edges are bi-directional
+        self.edges[from_node].append(to_node)
+        #self.edges[to_node].append(from_node)
+        self.weights[(from_node, to_node)] = weight
+        #self.weights[(to_node, from_node)] = weight
+
+
+def dijsktra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+
+    while current_node != end:
+        visited.add(current_node)
+        destinations = graph.edges[current_node]
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph.weights[(current_node, next_node)] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
+            else:
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return None
+        # next node is the destination with the lowest weight
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+
+    # Work back through destinations in shortest path
+    path = []
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    # Reverse path
+    path = path[::-1]
+    return path
+
+def poi_manips(poi, keys, doors):
+    key_to_key = {}
+    graph = Graph()
+    for node, adjacent_nodes in poi.items():
+        for adjacent_node, distance in adjacent_nodes:
+            graph.add_edge(node, adjacent_node, distance)
+
+    for key_start in list(keys) + ['ROOT']:
+        for key_end in keys:
+            if key_start == key_end:
+                continue
+            path = dijsktra(graph, key_start, key_end)
+            key_to_key[(key_start, key_end)] = path
+
+
+    return graph
+
 
 def poi_to_route(poi):
     num_keys = len([key for key in poi.keys() if key in string.ascii_lowercase])
@@ -204,7 +280,8 @@ def dfs_shortest_path(data):
 
 
 def find_shortest_path(data):
-    translated, poi, start_coords = translate_input(data)
+    translated, poi, start_coords, keys, doors = translate_input(data)
+    poi = poi_manips(poi, keys, doors)
     key_routes = poi_to_route(poi)
     shortest = min([sum([step[1] for step in route]) for route in key_routes])
     return shortest
@@ -256,10 +333,10 @@ if __name__ == '__main__':
         '########################',
     ]
     # Part 1
-    # assert find_shortest_path(sample_0) == 4
-    # assert find_shortest_path(sample_1) == 86
-    # assert find_shortest_path(sample_2) == 132
-    # #assert find_shortest_path(sample_3) == 136
-    #assert find_shortest_path(sample_4) == 81
+    #assert find_shortest_path(sample_0) == 4
+    #assert find_shortest_path(sample_1) == 86
+    #assert find_shortest_path(sample_2) == 132
+    assert find_shortest_path(sample_3) == 136
+    assert find_shortest_path(sample_4) == 81
 
     print(find_shortest_path(puzzle_input))
