99e6914c006ecfb5044069637dfa8e6c47ba674d
Brian Pandola <bpandola@hsdp.io>
2020-11-30 21:49:32 -0800

    Day 1, 2020 Cleaned up

--- 2020/day_01.py
+++ 2020/day_01.py
@@ -1,15 +1,31 @@
+def part_1(numbers, sum_to_find):
+    for i in range(len(numbers)):
+        num_1 = numbers[i]
+        for j in range(len(numbers)):
+            num_2 = numbers[j]
+            if num_1 + num_2 == sum_to_find:
+                return num_1 * num_2
 
 
+def part_2(numbers, sum_to_find):
+    for i in range(len(numbers)):
+        num_1 = numbers[i]
+        for j in range(len(numbers)):
+            num_2 = numbers[j]
+            for k in range(len(numbers)):
+                num_3 = numbers[k]
+                if num_1 + num_2 + num_3 == sum_to_find:
+                    return num_1 * num_2 * num_3
 
 
-puzzle_input = [int(i) for i in open('day_01.in').read().split('\n')]
-print(puzzle_input)
+if __name__ == '__main__':
+    puzzle_input = [int(i) for i in open('day_01.in').read().split('\n')]
+    sample_input = [1721, 979, 366, 299, 675, 1456]
 
-for i in range(len(puzzle_input)):
-    num_1 = puzzle_input[i]
-    for j in range(len(puzzle_input)):
-        num_2 = puzzle_input[j]
-        for k in range(len(puzzle_input)):
-            num_3 = puzzle_input[k]
-            if num_1 + num_2 + num_3 == 2020:
-                print("Indices ({}, {}): {}".format(i, j, num_1 * num_2  * num_3))
+    # Part 1
+    assert part_1(sample_input, 2020) == 514579
+    print(part_1(puzzle_input, 2020))
+
+    # Part 2
+    assert part_2(sample_input, 2020) == 241861950
+    print(part_2(puzzle_input, 2020))
