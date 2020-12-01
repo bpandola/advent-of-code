ab48c9d4ac6e28213f203fc6b2bef94a37189850
Brian Pandola <bpandola@hsdp.io>
2020-12-01 01:06:38 -0800

    Day 1, 2017 Generalized

--- 2017/day_01.py
+++ 2017/day_01.py
@@ -1,18 +1,7 @@
-def part_1(numbers):
+def solver(sequence, match_offset):
     accumulator = 0
-    sequence = numbers[:] + [numbers[0]]
-    for i in range(len(sequence) - 1):
-        if sequence[i] == sequence[i + 1]:
-            accumulator += sequence[i]
-    return accumulator
-
-
-def part_2(numbers):
-    accumulator = 0
-    sequence = numbers[:]
-    seq_len = len(sequence)
-    for i in range(seq_len):
-        if sequence[i] == sequence[(i + seq_len // 2) % seq_len]:
+    for i in range(len(sequence)):
+        if sequence[i] == sequence[(i + match_offset) % len(sequence)]:
             accumulator += sequence[i]
     return accumulator
 
@@ -29,8 +18,8 @@ if __name__ == '__main__':
 
     ]
     for inp, out in sample_data_1:
-        assert part_1(inp) == out
-    print(part_1(puzzle_input))
+        assert solver(inp, 1) == out
+    print(solver(puzzle_input, 1))
 
     # Part 2
     sample_data_2 = [
@@ -41,5 +30,5 @@ if __name__ == '__main__':
         ([1, 2, 1, 3, 1, 4, 1, 5], 4),
     ]
     for inp, out in sample_data_2:
-        assert part_2(inp) == out
-    print(part_2(puzzle_input))
+        assert solver(inp, len(inp) // 2) == out
+    print(solver(puzzle_input, len(puzzle_input) // 2))
