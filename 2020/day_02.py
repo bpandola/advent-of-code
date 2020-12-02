007747d2bcefb8b21ff9946e0150020d229893b7
Brian Pandola <bpandola@hsdp.io>
2020-12-01 22:48:12 -0800

    Day 2, 2020 Cleaned up

--- 2020/day_02.py
+++ 2020/day_02.py
@@ -1,5 +1,3 @@
-
-
 def parse_input(data):
     passwords = []
     for line in data:
@@ -15,31 +13,39 @@ def parse_input(data):
     return passwords
 
 
-def num_valid(passwords):
+def num_valid1(passwords):
     valid = 0
     for meta in passwords:
         password = meta['password']
         letter = meta['letter']
         count = password.count(letter)
-        if count >= meta['min'] and count <= meta['max']:
-            valid+=1
+        if meta['min'] <= count <= meta['max']:
+            valid += 1
     return valid
+
 
 def num_valid2(passwords):
     valid = 0
     for meta in passwords:
         password = meta['password']
         letter = meta['letter']
-        if int(password[meta['min']-1] == letter) ^ int(password[meta['max']-1]  == letter):
-            valid+=1
+        if int(password[meta['min'] - 1] == letter) ^ int(password[meta['max'] - 1] == letter):
+            valid += 1
     return valid
 
-puzzle_input = [i for i in open('day_02.in').read().split('\n')]
-sample_input = [
-    '1-3 a: abcde',
-'1-3 b: cdefg',
-'2-9 c: ccccccccc',
-]
-inp = parse_input(puzzle_input)
-print(num_valid(inp))
-print(num_valid2(inp))
+
+if __name__ == '__main__':
+    puzzle_input = [i for i in open('day_02.in').read().split('\n')]
+    sample_input = [
+        '1-3 a: abcde',
+        '1-3 b: cdefg',
+        '2-9 c: ccccccccc',
+    ]
+
+    # Part 1
+    assert num_valid1(parse_input(sample_input)) == 2
+    print(num_valid1(parse_input(puzzle_input)))
+
+    # Part 2
+    assert num_valid2(parse_input(sample_input)) == 1
+    print(num_valid2(parse_input(puzzle_input)))
