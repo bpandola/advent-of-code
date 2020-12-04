4b1e2cb0344a69023751c59e375dd85fd5f008b3
Brian Pandola <bpandola@hsdp.io>
2020-12-04 02:59:46 -0800

    Day 4, 2020 Cleaned up

--- 2020/day_04.py
+++ 2020/day_04.py
@@ -1,32 +1,33 @@
+def parse_input(filename):
+    lines = [line.replace('\n', ' ') for line in open(filename).read().split('\n\n')]
+    passports = []
+    for line in lines:
+        passport = {}
+        kvs = line.split()
+        for kv in kvs:
+            key, value = kv.split(':')
+            passport[key] = value
+        passports.append(passport)
+    return passports
 
-fields =  [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-    'cid',
-]
 
-def validator(name, value):
-    if name == 'byr':
+def check_constraint(key, value):
+    if key == 'byr':
         if len(value) != 4:
             return False
         if not (1920 <= int(value) <= 2002):
             return False
-    elif name == 'iyr':
+    elif key == 'iyr':
         if len(value) != 4:
             return False
         if not (2010 <= int(value) <= 2020):
             return False
-    elif name == 'eyr':
+    elif key == 'eyr':
         if len(value) != 4:
             return False
         if not (2020 <= int(value) <= 2030):
             return False
-    elif name == 'hgt':
+    elif key == 'hgt':
         if value.endswith('cm') or value.endswith('in'):
             if value.endswith('cm'):
                 if not (150 <= int(value[:-2]) <= 193):
@@ -36,18 +37,18 @@ def validator(name, value):
                     return False
         else:
             return False
-    elif name == 'hcl':
+    elif key == 'hcl':
         if value[0] != '#':
             return False
         valid_chars = '#0123456789abcdef'
         for c in value:
             if c not in valid_chars:
                 return False
-    elif name == 'ecl':
+    elif key == 'ecl':
         colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
         if value not in colors:
             return False
-    elif name == 'pid':
+    elif key == 'pid':
         if len(value) != 9:
             return False
         digits = '0123456789'
@@ -57,27 +58,34 @@ def validator(name, value):
     return True
 
 
-valid = 0
-p = [line for line in open('day_04.in').read().split('\n\n')]
-p = [line.replace('\n', ' ') for line in p]
-# print(p)
-for passport in p:
-    missing_field = 0
-    missing_cid = False
-    for field in fields:
-        if field not in passport:
-            missing_field += 1
-            if field == 'cid':
-                missing_cid = True
-    if missing_field == 1 and missing_cid or (missing_field == 0):
-        pass_fields = passport.split(' ')
-        for pf in pass_fields:
-            name, value = pf.split(':')
-            if not validator(name, value):
-                break
-        else:
-            valid += 1
+class PassportValidator:
+    REQUIRED_FIELDS = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+    OPTIONAL_FIELDS = ['cid']
 
-print(valid)
+    def __init__(self, check_constraints=False):
+        self.check_constraints = check_constraints
 
-# print(p)
+    def validate(self, passport):
+        if self.REQUIRED_FIELDS - set(passport.keys()):
+            return False
+        if self.check_constraints:
+            return all(check_constraint(k, v) for k, v in passport.items())
+        return True
+
+
+if __name__ == '__main__':
+    puzzle_input = parse_input('day_04.in')
+
+    # Part 1
+    sample_input = parse_input('day_04.in.sample_01')
+    pv = PassportValidator()
+    assert sum(pv.validate(p) for p in sample_input) == 2
+    print(sum(pv.validate(p) for p in puzzle_input))
+
+    # Part 2
+    sample_input_invalid = parse_input('day_04.in.sample_02a')
+    sample_input_valid = parse_input('day_04.in.sample_02b')
+    pv = PassportValidator(check_constraints=True)
+    assert sum(pv.validate(p) for p in sample_input_invalid) == 0
+    assert sum(pv.validate(p) for p in sample_input_valid) == 4
+    print(sum(pv.validate(p) for p in puzzle_input))
