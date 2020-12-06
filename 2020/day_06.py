6a19bbc98af42b06da424f65d16848f77350d675
Brian Pandola <bpandola@hsdp.io>
2020-12-06 02:15:02 -0800

    Day 6, 2020 Cleaned up

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -1,21 +1,22 @@
 from string import ascii_lowercase
-print(ascii_lowercase)
-lines = [line.split('\n') for line in open('day_06.in').read().split('\n\n')]
 
+if __name__ == '__main__':
+    puzzle_input = [line.split('\n') for line in open('day_06.in').read().split('\n\n')]
 
-total = 0
-for group in lines:
-    yeses = set()
-    for person in group:
-        for c in person:
-            yeses.add(c)
-    total += len(yeses)
-print(total)
+    # Part 1
+    total = 0
+    for group in puzzle_input:
+        yeses = set()
+        for person in group:
+            for answer in person:
+                yeses.add(answer)
+        total += len(yeses)
+    print(total)
 
-
-total = 0
-for group in lines:
-    for c in 'abcdefghijklmnopqrstuvwxyz':
-        if all([c in person for person in group]):
-            total += 1
-print(total)
+    # Part 2
+    total = 0
+    for group in puzzle_input:
+        for c in ascii_lowercase:
+            if all([c in person for person in group]):
+                total += 1
+    print(total)
