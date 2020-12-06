df3a886994d92b776b2c6e4bc28433716f45f946
Brian Pandola <bpandola@hsdp.io>
2020-12-06 02:40:36 -0800

    Day 5, 2020 Cleaned up

--- 2020/day_05.py
+++ 2020/day_05.py
@@ -1,48 +1,28 @@
-lines = [line for line in open('day_05.in').read().split('\n')]
+def decode_boarding_pass(bp):
+    binary = bp.translate(bp.maketrans('FBLR', '0101'))
+    id_ = int(binary, base=2)
+    row = id_ >> 3
+    col = id_ & int('111', base=2)
+    return row, col, id_
 
 
-#lines = ['FBFBBFFRLR']
-cabin =  {}
-highest = 0
-for code in lines:
+if __name__ == '__main__':
+    puzzle_input = [line for line in open('day_05.in').read().split('\n')]
 
-    row, col = 0, 0
-    left, right = (0, 127)
-    index = 0
-    while True:
-        c = code[index]
-        if c == 'F':
-            right = right - ((right - left + 1) // 2)
-        elif c == 'B':
-            left = left + ((right - left + 1) // 2)
-        if left == right:
-            row = right
-            break
-        index += 1
-    left, right = 0, 7
-    while True:
-        c = code[index]
-        if c == 'L':
-            right = right - ((right - left + 1) // 2)
-        elif c == 'R':
-            left = left + ((right - left + 1) // 2)
-        if left == right:
-            col = right
-            break
-        index += 1
-    seat_id = row * 8 + col
-    cabin[(row,col)]  = seat_id
-    if seat_id > highest:
-        highest = seat_id
-        print(highest)
-s = sorted(cabin.items())
-print(s)
-for y in range(128):
-    for x in range(8):
-        seat_id = cabin.get((y,x), 'missing')
-        if seat_id == 'missing':
-            seat_id = y * 8 + x
-            if seat_id-1 in cabin.values() and seat_id+1 in cabin.values():
-                cabin[(y, x)] = y*8+x
-                print('Seat ({},{}): {}'.format(y,x,cabin.get((y,x), 'missing')))
-                exit()
+    # Part 1
+    assert decode_boarding_pass('BFFFBBFRRR') == (70, 7, 567)
+    assert decode_boarding_pass('FFFBBBFRRR') == (14, 7, 119)
+    assert decode_boarding_pass('BBFFBBFRLL') == (102, 4, 820)
+    seats = []
+    highest = 0
+    for code in puzzle_input:
+        _, __, seat_id = decode_boarding_pass(code)
+        seats.append(seat_id)
+        highest = max(seat_id, highest)
+    print(highest)
+
+    # Part 2
+    sorted_seats = sorted(seats)
+    for i in range(128 * 8):
+        if i not in sorted_seats and i - 1 in sorted_seats and i + 1 in sorted_seats:
+            print(i)
