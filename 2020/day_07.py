78e80344437579cd670004c872792eb7f6aded62
Brian Pandola <bpandola@hsdp.io>
2020-12-07 01:14:46 -0800

    Day 7, 2020 Cleaned up

--- 2020/day_07.py
+++ 2020/day_07.py
@@ -1,48 +1,72 @@
 from functools import reduce
 
-puzzle_input = [line for line in open('day_07.in').read().split('\n')]
-bags = {}
-for line in puzzle_input:
-    outer, contents = line.split(' bags contain')
-    outer = outer.strip()
-    contents = contents.split(',')
-    bags[outer] = {}
-    for bag in contents:
-        content = bag.split(' ')
-        if content[1] == 'no':
-            continue
-        bag_name = content[2] + ' ' + content[3]
-        bags[outer][bag_name] = int(content[1])
 
-contains_gold = []
-def visit(bag, bags, path=None):
-    if path is None:
-        path = [bag]
-    for b in bags[bag]:
-        bpath = visit(b, bags, path + [b])
-        # Stop if we hit gold
-        if bpath[-1] == 'shiny gold':
-            for i in range(len(bpath)-1):
-                if bpath[i] not in contains_gold:
-                    contains_gold.append(bpath[i])
-    return path
-gold_paths = []
-def visit_gold(bag, bags, path=None):
-    if path is None:
-        path = [bag]
-    for b in bags[bag]:
-        bpath = visit_gold(b, bags, path + [b] + [bags[bag][b]])
-        # Avert thy gaze!!!
-        gold_paths.append(bpath[::2][1:])
-    return path
-containers = []
-for bag in bags:
-    path = visit(bag, bags)
-print(len(contains_gold))
+def parse_input(filename):
+    lines = [line for line in open(filename).read().split('\n')]
+    bags = {}
+    for line in lines:
+        outer, contents = line.split(' bags contain')
+        outer = outer.strip()
+        contents = contents.split(',')
+        bags[outer] = {}
+        for bag in contents:
+            content = bag.split(' ')
+            if content[1] == 'no':
+                continue
+            bag_name = content[2] + ' ' + content[3]
+            bags[outer][bag_name] = int(content[1])
+    return bags
 
-path = visit_gold('shiny gold', bags)
-total_bags = 0
-for bleh in gold_paths:
-    sub_total = reduce(lambda x, y: x * y, bleh)
-    total_bags += sub_total
-print(total_bags)
+
+def find_bags_that_contain_color(bag_color_to_find, bags):
+    contains_color = set()
+
+    def traverse(bag_color, path):
+        for b in bags[bag_color]:
+            temp_path = traverse(b, path + [b])
+            if temp_path[-1] == bag_color_to_find:
+                for i in range(len(temp_path) - 1):
+                    contains_color.add(temp_path[i])
+        return path
+
+    for bag in bags:
+        traverse(bag, [bag])
+    return contains_color
+
+
+def bagception(container_bag, bags):
+    # Count all the bags within the bags... within the container bag.
+    bag_counts = []
+
+    def traverse(bag_color, path):
+        for b in bags[bag_color]:
+            temp_path = traverse(b, path + [b] + [bags[bag_color][b]])
+            # Avert thy gaze!!!
+            # Basically, our path is bag color and counts:
+            # Example: ['shiny gold', 'dark olive', 1, 'faded blue', 3]
+            # So we step by 2 and drop the first item to get just the counts...
+            # Obviously, there must be a better way.  But this works!
+            bag_counts.append(temp_path[::2][1:])
+        return path
+
+    traverse(container_bag, [container_bag])
+    total_bags = 0
+    for counts  in bag_counts:
+        sub_total = reduce(lambda x, y: x * y, counts)
+        total_bags += sub_total
+    return total_bags
+
+
+if __name__ == '__main__':
+    puzzle_input = parse_input('day_07.in')
+
+    # Part 1
+    sample_input = parse_input('day_07.in.sample_01')
+    assert len(find_bags_that_contain_color('shiny gold', sample_input)) == 4
+    print(len(find_bags_that_contain_color('shiny gold', puzzle_input)))
+
+    # Part 2
+    assert bagception('shiny gold', sample_input) == 32
+    sample_input = parse_input('day_07.in.sample_02')
+    assert bagception('shiny gold', sample_input) == 126
+    print(bagception('shiny gold', puzzle_input))
