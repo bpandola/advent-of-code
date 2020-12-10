40585665e713a7bdaee2cd386b28167f78f0da0d
Brian Pandola <bpandola@hsdp.io>
2020-12-10 01:14:42 -0800

    Day 10, 2020 Cleaned up

--- 2020/day_10.py
+++ 2020/day_10.py
@@ -1,72 +1,64 @@
 from itertools import combinations
 from functools import reduce
-puzzle_input = [int(line) for line in open('day_10.in').read().split('\n')]
-
-adapters = puzzle_input[:]
-order = [(0,0)]
-#print(puzzle_input)
-joltage=0
-diff_seek = 1
-max_rating = max(adapters)+3
-adapters.append(max_rating)
-while adapters:
 
 
-
-    for index, adapter in enumerate(adapters):
-        if adapter-joltage == diff_seek:
-            joltage = adapter
-            order.append((adapter, diff_seek))
-            adapters.remove(adapter)
-            diff_seek = 0
-            break
-    diff_seek+=1
-    assert 0 < diff_seek < 4
+def parse_input(filename):
+    joltages = list(map(int, open(filename).read().split('\n')))
+    joltages.append(0)  # Add outlet joltage.
+    joltages.append(max(joltages) + 3)  # Add device joltage.
+    return sorted(joltages)
 
 
-print(order)
-print(joltage)
-diff_1 = len([(o, v) for (o,v) in order if v == 1])
-diff_3 = len([(o, v) for (o,v) in order if v == 3])
-print(diff_1*diff_3)
-
-for j in range(1,4):
-    print(list(combinations([5,6,7], j)))
-
-adapters = [a for (a,j) in order]
-chains = []
-index = 1 #  because we start at 0,0
-link_left = 0
-link_right = 0
-chains.append([adapters[0]])
-while index < len(adapters):
-    sub = [a for a in adapters[index:] if a - adapters[link_left] <= 3]
-    if len(sub) == 1:
-        chains.append(sub)
-        index+=1
-        link_left=index-1
-        link_right = index
-    elif len(sub) > 1:
-        length = len(sub)
-        link_right+= length
-        link_left+=length
-        index+=length
-        combos = []
-        for i in range(1,len(sub)+1):
-            pot = list(combinations(sub,i))
-            for p in pot:
-                if p[0] - adapters[link_left] <= 3:
-                    if adapters[link_right] - p[-1] <=3:
-                        combos.append(p)
-        chains.append(combos)
+def calculate_joltage_differences(joltages):
+    differences = []
+    for i in range(1, len(joltages)):
+        differences.append(joltages[i] - joltages[i - 1])
+    assert all([diff in [1, 3] for diff in differences])
+    return differences.count(1) * differences.count(3)
 
 
-print(chains)
-total = 1
-for i in chains:
-    total*=len(i)
-print(total)
+def calculate_num_adapter_arrangements(adapters):
+    # For any adapter that is required, we store it as a single item list.
+    # For any adapter sequences that can be rearranged, we store them as
+    # an array of all possible arrangements.
+    # Total arrangements can then be calculated by multiplying the lengths
+    # of all the lists together.
+    chains = [[adapters[0]]]
+    index = 1
+    while index < len(adapters):
+        potential_adapters = [a for a in adapters[index:] if a - adapters[index - 1] <= 3]
+        if len(potential_adapters) > 1:
+            index += len(potential_adapters)
+            combos = []
+            for i in range(1, len(potential_adapters) + 1):
+                possibilities = list(combinations(potential_adapters, i))
+                for p in possibilities:
+                    # We have to make sure that the first adapter in this
+                    # combination is within range of the previous adapter
+                    # and the last adapter in this combination is within
+                    # range of the next adapter in our list.
+                    if p[0] - adapters[index - 1] <= 3:
+                        if adapters[index] - p[-1] <= 3:
+                            combos.append(p)
+            chains.append(combos)
+        else:
+            chains.append(potential_adapters)
+            index += 1
+
+    return reduce(lambda x, y: x * y, [len(c) for c in chains])
 
 
+if __name__ == '__main__':
+    puzzle_input = parse_input('day_10.in')
+    sample_small = parse_input('day_10.in.sample_small')
+    sample_large = parse_input('day_10.in.sample_large')
 
+    # Part 1
+    assert calculate_joltage_differences(sample_small) == 35
+    assert calculate_joltage_differences(sample_large) == 220
+    print(calculate_joltage_differences(puzzle_input))
 
+    # Part 2
+    assert calculate_num_adapter_arrangements(sample_small) == 8
+    assert calculate_num_adapter_arrangements(sample_large) == 19208
+    print(calculate_num_adapter_arrangements(puzzle_input))
