f3c5c430140c0298ab0b243ed0b6f46532576df4
Brian Pandola <bpandola@hsdp.io>
2020-12-11 01:01:30 -0800

    Day 11, 2020 Cleaned up

--- 2020/day_11.py
+++ 2020/day_11.py
@@ -1,131 +1,69 @@
-from itertools import combinations
-from functools import reduce
-from collections import defaultdict
-
 def parse_input(filename):
-    lines = [line for line in  open(filename).read().split('\n')]
-    # Translate into a more convenient data structure.
-    # defaultdict[(x, y)] = data; default is '.'
-    translated = defaultdict(lambda: '.')
-
+    lines = open(filename).read().split('\n')
+    translated = {}
     for y, line in enumerate(lines):
         for x, data in enumerate(lines[y]):
             translated[(x, y)] = data
     return translated
 
-def run_simulation(area_map, ticks=0):
-    area = area_map.copy()
 
-    def num_occupied(coords):
-        x,y = coords
-        occupied = 0
-        if area[(x-1, y-1)] == '#':
-            occupied+=1
-        if area[(x, y - 1)] == '#':
-            occupied += 1
-        if area[(x+1, y - 1)] == '#':
-            occupied += 1
-        if area[(x-1, y+1)] == '#':
-            occupied+=1
-        if area[(x, y + 1)] == '#':
-            occupied += 1
-        if area[(x+1, y + 1)] == '#':
-            occupied += 1
-        if area[(x-1, y)] == '#':
-            occupied+=1
-        if area[(x+1, y)] == '#':
-            occupied += 1
-        return occupied
+dirs = [(-1, -1), (0, -1), (1, -1), (-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0)]
 
-    while True:
-        area_new = area.copy()
-        for coords in list(area.keys()):
-            if area[coords] == 'L' and num_occupied(coords) == 0:
-                area_new[coords] = '#'
-            elif area[coords] == '#' and num_occupied(coords) >= 4:
-                area_new[coords] = 'L'
 
-        for coords in list(area_new.keys()):
-            if area[coords] != area_new[coords]:
-                stopped = False
-                break
-        else:
-            stopped = True
+def num_adjacent_occupied(coords, area):
+    occupied = 0
+    x, y = coords
+    for dx, dy in dirs:
+        try:
+            if area[(x + dx, y + dy)] == '#':
+                occupied += 1
+        except KeyError:
+            pass  # Out of bounds.
+    return occupied
 
-        if stopped:
-            break
-        area = area_new.copy()
 
-    return len([(k,v) for k,v in area.items() if v == '#'])
-
-dirs = [
-
-(-1,-1),
-(0,-1),
-(1,-1),
-(-1,1),
-(0,1),
-(1,1),
-(-1,0),
-(1,0),
-]
-
-def run_simulation2(area_map, ticks=0):
-    area = area_map.copy()
-    minx = min(k[0] for k in area_map.keys())
-    maxx = max(k[0] for k in area_map.keys())
-    miny = min(k[1] for k in area_map.keys())
-    maxy = max(k[1] for k in area_map.keys())
-
-    def num_occupied(coords):
-        x,y = coords
-        occupied = 0
-
-        for i in range(8):
-            px, py = x, y
-            while True:
-                px += dirs[i][0]
-                py += dirs[i][1]
-                if area[(px,py)] == 'L':
+def num_visibly_occupied(coords, area):
+    occupied = 0
+    x, y = coords
+    for dx, dy in dirs:
+        px, py = x, y
+        while True:
+            px += dx
+            py += dy
+            try:
+                if area[(px, py)] == 'L':
                     break
                 if area[(px, py)] == '#':
                     occupied += 1
                     break
-                if px < minx or px > maxx:
-                    break
-                if py < miny or py > maxy:
-                    break
+            except KeyError:
+                break  # Out of bounds.
+    return occupied
 
 
-
-        return occupied
-
+def run_simulation(area_map, occupied_threshold, num_occupied):
+    area = area_map.copy()
     while True:
         area_new = area.copy()
         for coords in list(area.keys()):
-            if area[coords] == 'L' and num_occupied(coords) == 0:
+            if area[coords] == 'L' and num_occupied(coords, area) == 0:
                 area_new[coords] = '#'
-            elif area[coords] == '#' and num_occupied(coords) >= 5:
+            elif area[coords] == '#' and num_occupied(coords, area) >= occupied_threshold:
                 area_new[coords] = 'L'
-
-        for coords in list(area_new.keys()):
-            if area[coords] != area_new[coords]:
-                stopped = False
-                break
-        else:
-            stopped = True
-
-        if stopped:
+        if area_new == area:
             break
         area = area_new.copy()
-
-    return len([(k,v) for k,v in area.items() if v == '#'])
-
+    return list(area.values()).count('#')
 
 
-puzzle_input = parse_input('day_11.in')
+if __name__ == '__main__':
+    puzzle_input = parse_input('day_11.in')
+    sample_input = parse_input('day_11.in.sample')
 
+    # Part 1
+    assert run_simulation(sample_input, 4, num_adjacent_occupied) == 37
+    print(run_simulation(puzzle_input, 4, num_adjacent_occupied))
 
-
-print(run_simulation(puzzle_input))
-print(run_simulation2(puzzle_input))
+    # Part 2
+    assert run_simulation(sample_input, 5, num_visibly_occupied) == 26
+    print(run_simulation(puzzle_input, 5, num_visibly_occupied))
