1eca8c4fbb3dbadff66ddc041abdf05a1210bbb4
Brian Pandola <bpandola@hsdp.io>
2020-12-11 23:07:53 -0800

    Day 12, 2020 Cleaned up

--- 2020/day_12.py
+++ 2020/day_12.py
@@ -1,122 +1,98 @@
-import math
 def parse_input(filename):
     lines = [line.strip() for line in open(filename).read().split('\n')]
     instructions = []
     for line in lines:
-        op, value = line[0], int(line[1:])
-        instructions.append((op, value))
+        action, value = line[0], int(line[1:])
+        instructions.append((action, value))
     return instructions
 
-def calc_manhattan_dist(instructions):
-    delta_x = {'L': -1, 'R': 1, 'U': 0, 'D': 0}
-    delta_y = {'L': 0, 'R': 0, 'U': 1, 'D': -1}
-    robot_to_direction_map = {'^': 'U', 'v': 'D', '<': 'L', '>': 'R', }
-    # current_direction = robot_to_direction_map[area_map[robot_pos]]
-    x, y = 0,0
-    heading = 'R'
 
-    headings_by_turn = {
-        'R': {
-            'L': {90: 'U', 180: 'R', 270: 'D'},
-            'R': {90: 'D', 180: 'L', 270: 'U'},
-            'U': {90: 'R', 180: 'D', 270: 'L'},
-            'D': {90: 'L', 180: 'U', 270: 'R'},
+def nav_with_assumed_instructions(instructions):
+    headings = ['N', 'E', 'S', 'W']
+    delta_x = {'N': 0, 'S': 0, 'W': -1, 'E': 1}
+    delta_y = {'N': 1, 'S': -1, 'W': 0, 'E': 0}
 
-        },
-        'L': {
-            'L': {90: 'D', 180: 'R', 270: 'U'},
-            'R': {90: 'U', 180: 'L', 270: 'D'},
-            'U': {90: 'L', 180: 'D', 270: 'R'},
-            'D': {90: 'R', 180: 'U', 270: 'L'},
+    heading = 'E'
+    x, y = 0, 0
 
-        }
-    }
-
-    for op, value in instructions:
-        if op == 'N':
-            y+=1*value
-        elif op == 'S':
-            y-=1*value
-        elif op == 'E':
-            x+=1*value
-        elif op == 'W':
-            x-=1*value
-        elif op == 'F':
-            x+=delta_x[heading]*value
-            y+=delta_y[heading]*value
-        elif op == 'L' or op == 'R':
-            heading = headings_by_turn[op][heading][value]
-        else:
-            raise RuntimeError('bad op')
+    for action, value in instructions:
+        if action == 'N':
+            y += 1 * value
+        elif action == 'S':
+            y -= 1 * value
+        elif action == 'E':
+            x += 1 * value
+        elif action == 'W':
+            x -= 1 * value
+        elif action == 'F':
+            x += delta_x[heading] * value
+            y += delta_y[heading] * value
+        elif action == 'L':
+            dir_index = headings.index(heading)
+            for _ in range(value // 90):
+                dir_index = (dir_index + 3) % 4
+            heading = headings[dir_index]
+        elif action == 'R':
+            dir_index = headings.index(heading)
+            for _ in range(value // 90):
+                dir_index = (dir_index + 1) % 4
+            heading = headings[dir_index]
 
     return abs(x) + abs(y)
 
 
-def calc_waypoint_dist(instructions):
-    delta_x = {'L': -1, 'R': 1, 'U': 0, 'D': 0}
-    delta_y = {'L': 0, 'R': 0, 'U': -1, 'D': 1}
-    robot_to_direction_map = {'^': 'U', 'v': 'D', '<': 'L', '>': 'R', }
-    # current_direction = robot_to_direction_map[area_map[robot_pos]]
-    x, y = 0,0
+def nav_with_correct_instructions(instructions):
     way_x, way_y = 10, 1
-    heading = 'R'
-    way_headings = ['R','U']
+    x, y = 0, 0
 
-    headings_by_turn = {
-        'R': {
-            'L': {90: 'U', 180: 'R', 270: 'D'},
-            'R': {90: 'D', 180: 'L', 270: 'U'},
-            'U': {90: 'R', 180: 'D', 270: 'L'},
-            'D': {90: 'L', 180: 'U', 270: 'R'},
-
-        },
-        'L': {
-            'L': {90: 'D', 180: 'R', 270: 'U'},
-            'R': {90: 'U', 180: 'L', 270: 'D'},
-            'U': {90: 'L', 180: 'D', 270: 'R'},
-            'D': {90: 'R', 180: 'U', 270: 'L'},
-
-        }
-    }
-
-    for op, value in instructions:
-        if op == 'N':
-            way_y+=1*value
-        elif op == 'S':
-            way_y-=1*value
-        elif op == 'E':
-            way_x+=1*value
-        elif op == 'W':
-            way_x-=1*value
-        elif op == 'F':
-            dx = way_x-x
-            dy = way_y-y
-            x+=dx*value
-            y+=dy*value
-            way_x+=dx*value
-            way_y+=dy*value
-        elif op == 'L' or op == 'R':
-            px, py = way_x, way_y
-            ox, oy = x, y
-            angle = math.radians(-value if op == 'R' else value)
-            qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-            qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-            way_x, way_y  = qx, qy
-        else:
-            raise RuntimeError('bad op')
+    for action, value in instructions:
+        if action == 'N':
+            way_y += 1 * value
+        elif action == 'S':
+            way_y -= 1 * value
+        elif action == 'E':
+            way_x += 1 * value
+        elif action == 'W':
+            way_x -= 1 * value
+        elif action == 'F':
+            x += way_x * value
+            y += way_y * value
+        # Originally, I looked up the math for rotating a point.
+        # This works, but it leaves us with a floating point number
+        # that is *very* close to being wrong.
+        # elif action == 'L' or action == 'R':
+        #     angle = math.radians(-value if action == 'R' else value)
+        #     tx = math.cos(angle) * way_x - math.sin(angle) * way_y
+        #     ty = math.sin(angle) * way_x + math.cos(angle) * way_y
+        #     way_x, way_y = tx, ty
+        #
+        # I got the following from Jonathan Paulson. Very cool trick!
+        # Complex numbers are a good way to think about rotations!
+        # Think of the point (x,y) as the complex number x+iy
+        # Remember i^2=-1. Multiplying by i is the same as rotating 90 degrees.
+        # Why? Note i^4 = (-1)^2 = 1, so multiplying by i four times does nothing.
+        # i^2 = -1, so multiplying by i flips you around the x and y axes (which is a 180-degree rotation).
+        # (x,y)*i = (x+iy)*i = ix+i^2y = -y+ix = (-y,x)
+        # (x,y)*i^3 = (x+iy)*i^3 = i^3x+i^4y = y - ix = (y,-x)
+        # https://calcworkshop.com/transformations/rotation-rules/
+        elif action == 'L':
+            for _ in range(value // 90):
+                way_x, way_y = -way_y, way_x
+        elif action == 'R':
+            for _ in range(value // 90):
+                way_x, way_y = way_y, -way_x
 
     return abs(x) + abs(y)
+
+
 if __name__ == '__main__':
     puzzle_input = parse_input('day_12.in')
     sample_input = parse_input('day_12.in.sample')
 
     # Part 1
-    assert calc_manhattan_dist(sample_input) == 25
-    print(calc_manhattan_dist(puzzle_input))
+    assert nav_with_assumed_instructions(sample_input) == 25
+    print(nav_with_assumed_instructions(puzzle_input))
 
     # Part 1
-    assert calc_waypoint_dist(sample_input) == 286
-    print(calc_waypoint_dist(puzzle_input))
-
-
-
+    assert nav_with_correct_instructions(sample_input) == 286
+    print(nav_with_correct_instructions(puzzle_input))
