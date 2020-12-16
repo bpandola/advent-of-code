bf1f3feee93fa2e5583b78db865ff9e3b4e91ae5
Brian Pandola <bpandola@gmail.com>
2020-12-16 00:01:02 -0800

    Day 16, 2020 Cleaned up

--- 2020/day_16.py
+++ 2020/day_16.py
@@ -1,102 +1,108 @@
+from collections import defaultdict
+
 
 class Range:
 
     def __init__(self, range_):
-        self.min = int(range_.split('-')[0])
-        self.max = int(range_.split('-')[1])
+        min_, max_ = range_
+        self.min = min_
+        self.max = max_
+
 
 class Rule:
 
-    def __init__(self, name, range):
+    def __init__(self, name, ranges):
         self.name = name
-        self.ranges= []
-        for r in range.split(' or '):
+        self.ranges = []
+        for r in ranges:
             self.ranges.append(Range(r))
-
 
 
 def parse_input(filename):
     blobs = [blob for blob in open(filename).read().split('\n\n')]
-    rules, ticket, nearby = blobs[0], blobs[1], blobs[2]
+    rules_raw, ticket, nearby = blobs[0], blobs[1], blobs[2]
     parsed_rules = []
-    for rule in rules.split('\n'):
+    for rule in rules_raw.split('\n'):
         name, range_ = rule.split(': ')
-        parsed_rules.append(Rule(name, range_))
+        ranges = []
+        for r in range_.split(' or '):
+            ranges.append((int(r.split('-')[0]), int(r.split('-')[1])))
+        parsed_rules.append(Rule(name, ranges))
+    ticket = list(map(int, ticket.split('\n')[1].split(',')))
     tickets_nearby = []
     for near in nearby.split('\n'):
         if near.startswith('nearby'):
             continue
-        tickets_nearby.append(near.split(','))
+        tickets_nearby.append(list(map(int, near.split(','))))
+    return parsed_rules, ticket, tickets_nearby
 
 
-
-
-
-    return parsed_rules, tickets_nearby
-
-
-if __name__ == '__main__':
-    puzzle_input = parse_input('day_16.in')
-    sample_input = parse_input('day_16.in.sample')
-
-    rules, tickets_nearby =  puzzle_input
-    total=0
-    for ticket in tickets_nearby[:]:
+def validate_tickets(tickets, constraints):
+    tickets_validated = tickets[:]
+    ticket_error_rate = 0
+    for ticket in tickets:
         for value in ticket:
             invalid = True
-            for rule in rules:
-                for r in rule.ranges:
-                    if r.min <= int(value) <= r.max:
+            for constraint in constraints:
+                for r in constraint.ranges:
+                    if r.min <= value <= r.max:
                         invalid = False
             if invalid:
-                total += int(value)
-                tickets_nearby.remove(ticket)
-    print(total)
-    my_ticket = [79,193,53,97,137,179,131,73,191,139,197,181,67,71,211,199,167,61,59,127]
+                ticket_error_rate += value
+                tickets_validated.remove(ticket)
+    return tickets_validated, ticket_error_rate
 
 
-    rule_names = [r.name for r in rules]
-    name_order = []
-    possible = {}
-    position = 0
-    while position < len(rule_names):
-        #position = len(name_order)
-        #print(name_order)
-        for name in [n for n in rule_names if n not in name_order]:
-            possibilities = []
-            rule = next(r for r in rules if r.name == name)
+def ascertain_field_order(tickets_validated, constraints):
+    possible = defaultdict(list)
+    for constraint in constraints:
+        for position in range(len(constraints)):
             valid = True
-            for ticket in tickets_nearby:
+            for ticket in tickets_validated:
                 value = int(ticket[position])
-                if not any([r.min <= int(value) <= r.max for r in rule.ranges]):
+                if not any([r.min <= int(value) <= r.max for r in constraint.ranges]):
                     valid = False
                 if not valid:
                     break
             if valid:
-                if name not in possible:
-                    possible[name] = []
-                possible[name].append(position)
-                # break
-        position += 1
-
-    ordered = [True for _ in range(len(rule_names))]
-
-    while True:
-        for k, v in possible.items():
-            if len(v) == 1:
-                value = v[0]
+                possible[constraint.name].append(position)
+    # Loop through the possibilities until we only have one possible
+    # field name left for each position.  If we encounter a possible
+    # positions array with only one value, we know that is correct,
+    # so we remove that value from all other possible position arrays.
+    while any(len(v) != 1 for v in possible.values()):
+        for possible_positions in possible.values():
+            if len(possible_positions) == 1:
+                value = possible_positions[0]
                 for j in possible:
                     if len(possible[j]) > 1 and value in possible[j]:
                         possible[j].remove(value)
-        if all(len(v) == 1 for v in possible.values()):
-            break
-    name_order = ['' for _ in range(len(possible))]
-    for k,v in possible.items():
-        name_order[v[0]] = k
-    print(name_order)
+    # Now we have dictionary of field names to positions, which we
+    # want to translate to an ordered array of field names.
+    return [item[0] for item in sorted(possible.items(), key=lambda item: item[1][0])]
+
+
+if __name__ == '__main__':
+    puzzle_input = parse_input('day_16.in')
+
+    # Part 1
+    sample_input = parse_input('day_16.in.sample_01')
+    _, error_rate = validate_tickets(sample_input[2], sample_input[0])
+    assert error_rate == 71
+
+    valid_tickets, error_rate = validate_tickets(puzzle_input[2], puzzle_input[0])
+    print(error_rate)
+
+    # Part 2
+    sample_input = parse_input('day_16.in.sample_02')
+    valid_sample_tickets, _ = validate_tickets(sample_input[2], sample_input[0])
+    fields = ascertain_field_order(valid_sample_tickets, sample_input[0])
+    assert fields == ['row', 'class', 'seat']
+
+    rules, my_ticket, _ = puzzle_input
+    field_order = ascertain_field_order(valid_tickets, rules)
     total = 1
-    for i, name in enumerate(name_order):
-        if name.startswith('departure'):
+    for i, field in enumerate(field_order):
+        if field.startswith('departure'):
             total *= my_ticket[i]
     print(total)
-
