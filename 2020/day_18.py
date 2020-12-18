a676b2cc6963e7eb3efe63c867d00411cb3603d9
Brian Pandola <bpandola@gmail.com>
2020-12-18 00:22:36 -0800

    Day 18, 2020 Cleaned up

--- 2020/day_18.py
+++ 2020/day_18.py
@@ -1,22 +1,7 @@
-import ast
-def parse_input(filename):
-    lines = open(filename).read().split('\n')
-    translated = {}
-    for y, line in enumerate(lines):
-        for x, data in enumerate(lines[y]):
-            translated[(x, y, 0, 0)] = data
-    return translated
-
-def calculate2(expression):
-    ex = [c for c in expression]
-    i = 0
+def evaluate(expression):
     open_parens = []
     close_parens = []
-    tokens = []
-    operand = None
-    left = None
-    right = None
-    total = 0
+    left, right, operand = None, None, None
     for index, c in enumerate(expression):
         if c == '(':
             open_parens.append(index)
@@ -26,13 +11,13 @@ def calculate2(expression):
         if len(open_parens) == len(close_parens) and len(open_parens) != 0:
             start = open_parens[0] + 1
             stop = close_parens[-1]
-            temp = calculate2(expression[start:stop])
+            temp = evaluate(expression[start:stop])
             if left is None:
                 left = int(temp)
             else:
                 right = int(temp)
             open_parens = []
-            close_parens =[]
+            close_parens = []
             if right is None:
                 continue
         if len(open_parens) != len(close_parens):
@@ -51,8 +36,12 @@ def calculate2(expression):
                 continue
             operand = c
 
-
-        if left is not None and right is not None and operand is not None and not open_parens:
+        if (
+                left is not None and
+                right is not None and
+                operand is not None
+                and not open_parens
+        ):
             if operand == '-':
                 left = left - right
             elif operand == '+':
@@ -64,77 +53,65 @@ def calculate2(expression):
     return left
 
 
-
-def preparse(expression):
-    # put parens around all plus signs
-    # print(preparse("1 + (2 * 3) + (4 * (5 + 6))"))
+def new_rules(expression):
+    # Put parentheses around all plus signs to ensure order of operations.
     ex = [c for c in expression]
-    open_parens = []
-    close_parens = []
-    index = 0
 
-    def find_index(start, left=True):
-        step = -1 if left else 1
-        stop = 0 if left else len(ex)
-        parens = 0
+    def find_parens(start, is_left_op=True):
+        step = -1 if is_left_op else 1
+        stop = 0 if is_left_op else len(ex)
+        i = parens = 0
         for i in range(start, stop, step):
             c = ex[i]
             if c == ')':
-                parens-=1
-                if not left and not parens:
-                    return i
+                parens -= 1
+                if not is_left_op and not parens:
+                    break
             elif c == '(':
-                parens+=1
-                if left and not parens:
-                    return i
+                parens += 1
+                if is_left_op and not parens:
+                    break
             elif c.isdigit() and not parens:
-                return i
+                break
         else:
-            return i-1 if left else i
+            i = i - 1 if is_left_op else i
+        return i
 
-
-
+    index = 0
     while True:
         c = ex[index]
-        # if c == '(':
-        #     open_parens.append(index)
-        # elif c == ')':
-        #     close_parens.append(index)
         if c == '+':
-            left = find_index(index, left=True)
-            right = find_index(index, left=False)
+            left = find_parens(index, is_left_op=True)
+            right = find_parens(index, is_left_op=False)
             ex.insert(left, '(')
-            ex.insert(right+2, ')')
-            open_parens = close_parens = 0
-            index+=1
-
-        index+=1
+            ex.insert(right + 2, ')')  # Have to account for left being added.
+            index += 1
+        index += 1
         if index >= len(ex):
             break
 
     return ''.join(ex)
 
 
-
-def calculate(expression):
-    p = ast.parse(expression)
-    for node in ast.walk(p):
-        print(str(node))
-
-
-
 if __name__ == '__main__':
     puzzle_input = [line for line in open('day_18.in').read().split('\n')]
-    #sample_input = parse_input('day_18.in.sample')
 
-    assert(calculate2(preparse("1 + 2 * 3 + 4 * 5 + 6"))) == 231
-    assert(calculate2(preparse("1 + (2 * 3) + (4 * (5 + 6))"))) == 51
-    assert (calculate2(preparse("2 * 3 + (4 * 5)"))) == 46
-    assert (calculate2(preparse("5 + (8 * 3 + 9 + 3 * 4 * 3)"))) == 1445
-    assert (calculate2(preparse("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))) == 669060
+    # Part 1
+    def p1(x): return evaluate(x)
+    assert p1("1 + 2 * 3 + 4 * 5 + 6") == 71
+    assert p1("1 + (2 * 3) + (4 * (5 + 6))") == 51
+    assert p1("2 * 3 + (4 * 5)") == 26
+    assert p1("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
+    assert p1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
+    assert p1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
+    print(sum([p1(exp) for exp in puzzle_input]))
 
-    assert(calculate2(preparse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))) == 23340
-
-    assert calculate2("1 + 2 * 3 + 4 * 5 + 6") == 71
-    print(calculate2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
-    print(sum([calculate2(preparse(exp)) for exp in puzzle_input]))
+    # Part 2
+    def p2(x): return evaluate(new_rules(x))
+    assert p2("1 + 2 * 3 + 4 * 5 + 6") == 231
+    assert p2("1 + (2 * 3) + (4 * (5 + 6))") == 51
+    assert p2("2 * 3 + (4 * 5)") == 46
+    assert p2("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+    assert p2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669060
+    assert p2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
+    print(sum([p2(exp) for exp in puzzle_input]))
