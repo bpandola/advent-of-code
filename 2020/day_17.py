bba42ad5e0692ce46fe105a02e99affea543f778
Brian Pandola <bpandola@gmail.com>
2020-12-18 19:18:31 -0800

    Day 17, 2020 Complete(ly sloooow)

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -1,91 +1,77 @@
-
-
-def parse_input(filename):
+def parse_input(filename, dimensions):
     lines = open(filename).read().split('\n')
     translated = {}
     for y, line in enumerate(lines):
         for x, data in enumerate(lines[y]):
-            translated[(x, y, 0)] = data
+            coords = [x, y] + [0] * (dimensions - 2)
+            translated[tuple(coords)] = data
     return translated
 
 
-dirs = [
-    (-1, -1, 0), (0, -1, 0), (1, -1, 0), (-1, 1, 0), (0, 1, 0), (1, 1, 0), (-1, 0, 0), (1, 0, 0),
+def get_neighbor_deltas(dimensions):
+    dirs = []
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            for z in range(-1, 2):
+                for w in [0] if dimensions == 3 else range(-1, 2):
+                    if any([n for n in (x, y, z, w) if n != 0]):
+                        coords = [x, y, z, w]
+                        dirs.append(tuple(coords[:dimensions]))
+    return dirs
 
-    (-1, -1, 1), (0, -1, 1), (1, -1, 1), (-1, 1, 1), (0, 1, 1), (1, 1, 1), (-1, 0, 1), (1, 0, 1), (0, 0, 1),
 
-    (-1, -1, -1), (0, -1, -1), (1, -1, -1), (-1, 1, -1), (0, 1, -1), (1, 1, -1), (-1, 0, -1), (1, 0, -1), (0, 0, -1)
-
-]
-
-
-def num_adjacent_occupied(coords, area):
+def num_adjacent_occupied(coords, area, deltas):
     occupied = 0
-    x, y, z = coords
-    for dx, dy, dz in dirs:
+    for delta in deltas:
+        translated = []
+        for i in range(len(coords)):
+            translated.append(coords[i] + delta[i])
+
         try:
-            if area[(x + dx, y + dy, z + dz)] == '#':
+            if area[tuple(translated)] == '#':
                 occupied += 1
         except KeyError:
             pass  # Out of bounds.
     return occupied
 
 
-def num_visibly_occupied(coords, area):
-    occupied = 0
-    x, y, z = coords
-    for dx, dy in dirs:
-        px, py = x, y
-        while True:
-            px += dx
-            py += dy
-            try:
-                if area[(px, py)] == 'L':
-                    break
-                if area[(px, py)] == '#':
-                    occupied += 1
-                    break
-            except KeyError:
-                pass  # Out of bounds.
-    return occupied
-
-
-def run_simulation(area_map):
+def run_simulation(area_map, num_steps=6, dimensions=3):
+    deltas = get_neighbor_deltas(dimensions)
     area = area_map.copy()
-    for i in range(6):
+    start, stop = -1, max(max(list(area.keys()))) + 2
+    for i in range(num_steps):
         area_new = area.copy()
-        c_min = min(area.keys())
-        c_max = max(area.keys())
-        # for x in range(c_min[0]-5, c_max[0]+5,1):
-        #     for y in range(c_min[1] - 5, c_max[1] + 5,1):
-        #         for z in range(c_min[2] - 5, c_max[2] + 5, 1):
-        for x in range(-10, 25, 1):
-            for y in range(-10, 25, 1):
-                for z in range(-10, 25, 1):
-                    coords  = x, y, z
-                    neighbors = num_adjacent_occupied(coords, area)
-                    if coords not in area:
-                        area[coords] = '.'
-                    try:
-                        if area[coords] == '#':
-                            if neighbors not in [2,3]:
-                                area_new[coords] = '.'
-                        elif area[coords] == '.' and neighbors == 3:
-                            area_new[coords] = '#'
-                    except KeyError:
-                        pass
+        for x in range(start, stop):
+            for y in range(start, stop):
+                for z in range(start, stop):
+                    for w in [0] if dimensions == 3 else range(start, stop):
+                        coords = tuple([x, y, z, w][:dimensions])
+                        num_neighbors = num_adjacent_occupied(coords, area, deltas)
+                        if coords not in area:
+                            area[coords] = '.'
+                        try:
+                            if area[coords] == '#':
+                                if num_neighbors not in [2, 3]:
+                                    area_new[coords] = '.'
+                            elif area[coords] == '.' and num_neighbors == 3:
+                                area_new[coords] = '#'
+                                if start in coords:
+                                    start -= 1
+                                if stop - 1 in coords:
+                                    stop += 1
+                        except KeyError:
+                            pass
         area = area_new.copy()
     return list(area.values()).count('#')
 
 
 if __name__ == '__main__':
-    puzzle_input = parse_input('day_17.in')
-    sample_input = parse_input('day_17.in.sample')
+    print('Be patient.  This is gonna take a couple minutes...')
 
     # Part 1
-    print(run_simulation(sample_input))
-    print(run_simulation(puzzle_input))
+    assert run_simulation(parse_input('day_17.in.sample', 3)) == 112
+    print(run_simulation(parse_input('day_17.in', 3)))
 
-    # Part 2
-    # assert run_simulation(sample_input, 5, num_visibly_occupied) == 26
-    # print(run_simulation(puzzle_input, 5, num_visibly_occupied))
+    # Part 1
+    assert run_simulation(parse_input('day_17.in.sample', 4), dimensions=4) == 848
+    print(run_simulation(parse_input('day_17.in', 4), dimensions=4))
