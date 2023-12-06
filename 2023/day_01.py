9582886ad233402a192d64e02e89f578b4e6d94e
Brian Pandola <bpandola@gmail.com>
2023-12-06 14:27:27 -0800

    Day 1, 2023 Part 2

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -11,15 +11,70 @@ def parse_calibration_values_v1(blursed_inputs):
                 digits.append(character)
         value = int(str(digits[0]+str(digits[-1])))
         values.append(value)
+    return values
 
+def parse_calibration_values_v2(blursed_inputs):
+    words_to_digits = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
+
+    values = []
+    for line in blursed_inputs:
+        digits = []
+        # Find first digit
+        for index, character in enumerate(line):
+            if character.isdigit():
+                digits.append(character)
+                break
+            for word in words_to_digits:
+                found_word = False
+                if line[index:].find(word) == 0:
+                    digits.append(words_to_digits[word])
+                    found_word = True
+                if found_word:
+                    break
+            if digits:
+                break
+        # Find second digit
+        line_reversed = reversed(line)
+        for index, character in enumerate(line_reversed):
+            if character.isdigit():
+                digits.append(character)
+                break
+            for word in words_to_digits:
+                found_word = False
+                if line[-(index+1):].find(word) == 0:
+                    digits.append(words_to_digits[word])
+                    found_word = True
+                if found_word:
+                    break
+            if len(digits) == 2:
+                break
+
+
+
+
+        value = int(str(digits[0])+str(digits[1]))
+        values.append(value)
     return values
 
 if __name__ == '__main__':
     puzzle_input = parse_input('day_01.in')
-    sample_input = parse_input('day_01.in.sample_01')
 
     # Part 1
+    sample_input = parse_input('day_01.in.sample_01')
     assert sum(parse_calibration_values_v1(sample_input)) == 142
     print(sum(parse_calibration_values_v1(puzzle_input)))
 
     # Part 2
+    sample_input = parse_input('day_01.in.sample_02')
+    assert sum(parse_calibration_values_v2(sample_input)) == 281
+    print(sum(parse_calibration_values_v2(puzzle_input)))
