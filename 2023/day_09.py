1527c734895f1d94746d0eeb0b235c89ddc20207
Brian Pandola <bpandola@gmail.com>
2023-12-08 22:08:31 -0800

    Day 9, 2023 Cleaned Up

--- 2023/day_09.py
+++ 2023/day_09.py
@@ -1,71 +1,53 @@
 def parse_input(filename):
-    data = []
     lines = open(filename).read().split('\n')
-    for line in lines:
-        data.append([int(i) for i in line.split()])
+    data = [[int(i) for i in line.split()] for line in lines]
     return data
 
-def find_next_value(sequence):
-    sequences = []
-    sequences.append(sequence[:])
+
+def extrapolate_history(history):
+    sequences = [history[:]]
     n = 0
     while True:
-        new_seq = []
-        seq = sequences[n]
-        for i in range(len(seq)-1):
-            new_seq.append(seq[i+1]-seq[i])
-        sequences.append(new_seq)
-        if sum(new_seq) == 0:
+        sequence_new = []
+        sequence_cur = sequences[n]
+        for i in range(len(sequence_cur) - 1):
+            sequence_new.append(sequence_cur[i + 1] - sequence_cur[i])
+        sequences.append(sequence_new)
+        if sum(sequence_new) == 0:  # All zeroes, we out.
             break
-        n+=1
-    # Walk sequences backward to get next
-    for i in range(len(sequences)-1, 0, -1):
-        s = sequences[i]
-        t = sequences[i-1]
-        t.append(t[-1]+s[-1])
-    return sequences[0][-1]
-
-def find_next_values(sequences):
-    next_nums = []
-    for sequence in sequences:
-        next_num = find_next_value(sequence)
-        next_nums.append(next_num)
-    return sum(next_nums)
+        n += 1
+    # Walk sequences in reverse to extrapolate previous/next values.
+    for i in range(len(sequences) - 1, 0, -1):
+        sequence_prev = sequences[i]
+        sequence = sequences[i - 1]
+        # Extrapolate next value.
+        sequence.append(sequence[-1] + sequence_prev[-1])
+        # Extrapolate previous value.
+        sequence.insert(0, sequence[0] - sequence_prev[0])
+    return sequences[0]
 
 
-def find_prev_value(sequence):
-    sequences = []
-    sequences.append(sequence[:])
-    n = 0
-    while True:
-        new_seq = []
-        seq = sequences[n]
-        for i in range(len(seq)-1):
-            new_seq.append(seq[i+1]-seq[i])
-        sequences.append(new_seq)
-        if sum(new_seq) == 0:
-            break
-        n+=1
-    # Walk sequences backward to get prev
-    for i in range(len(sequences)-1, 0, -1):
-        s = sequences[i]
-        t = sequences[i-1]
-        t.insert(0, t[0]-s[0])
-    return sequences[0][0]
+def find_next_values(histories):
+    next_values = []
+    for history in histories:
+        new_sequence = extrapolate_history(history)
+        next_values.append(new_sequence[-1])
+    return sum(next_values)
 
-def find_prev_values(sequences):
-    next_nums = []
-    for sequence in sequences:
-        next_num = find_prev_value(sequence)
-        next_nums.append(next_num)
-    return sum(next_nums)
+
+def find_prev_values(histories):
+    next_values = []
+    for history in histories:
+        new_sequence = extrapolate_history(history)
+        next_values.append(new_sequence[0])
+    return sum(next_values)
 
 
 if __name__ == '__main__':
+    sample_input = parse_input('day_09.in.sample')
     puzzle_input = parse_input('day_09.in')
 
     # Part 1
-    sample_input = parse_input('day_09.in.sample')
     assert find_next_values(sample_input) == 114
     print(find_next_values(puzzle_input))
 
